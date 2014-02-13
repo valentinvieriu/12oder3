c28d882dcb8fec0cd37754c85af10beedccef120
Valentin Vieriu <vvieriu@sapient.com>
2014-02-13 12:09:29 +0100

    we found points for right answers

--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -7,19 +7,26 @@ angular.module('12oder3App')
             $routeParams,
             $location,
             Localstorage,
-            data
+            data,
+            _
         ) {
             var qid                = $routeParams.qid;
-            var userId        = Localstorage.get('uuid');
+            var userId             = Localstorage.get('uuid');
             var stop;
             $scope.timeOut         = data.timeOut;
+            $scope.userScore       = data.fBase.currentUser.$child('score');
             $scope.currentQuestion = data.fBase.questions.$child(qid);
             $scope.currentUser     = data.fBase.currentUser;
             $scope.activeQuestion  = data.fBase.activeQuestion;
             $scope.playTimer       = data.fBase.playTimer.$value;
+            $scope.timeOver        = false;
 
-            data.fBase.playTimer.$on('change',function(newTime){
+            data.fBase.playTimer.$on('change',function(){
                 $scope.playTimer = data.fBase.playTimer.$value;
+                if ($scope.playTimer === 0) {
+                    $scope.isItRight = testResponse();
+                    $scope.timeOver = true;
+                };
 
             });
             // $scope.activeQuestion.$on('loaded',function(){
@@ -37,9 +44,28 @@ angular.module('12oder3App')
                 });
                 $scope.lastVote = vote;
             }
+            // var savedScore = false;
 
-            $scope.isItRight = function(){
-            	return $scope.lastVote == $scope.currentQuestion.correctAnswer;
+            function testResponse(){
+                var result = false ;
+                
+                if($scope.lastVote == $scope.currentQuestion.correctAnswer) {
+                    result = true;
+                    var oldScore = $scope.userScore.$value || 0;
+                    console.log(new Date().getTime(),'oldScore',oldScore,$scope.lastVote,$scope.currentQuestion.correctAnswer );
+
+                    var newScore =  oldScore + 1;
+                    console.log('newScore',newScore, new Date().getTime());
+                    $scope.userScore.$set(newScore);
+                    // savedScore = true;
+                    
+                    
+                    // $scope.userScore.$set(newScore);
+                }
+                else {
+                    console.log('Wrong Answer!');
+                }
+            	return result;
             }
             
             
