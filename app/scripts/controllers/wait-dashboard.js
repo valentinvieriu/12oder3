fa4da9eb84de7ca127d27b4885d1884125423500
Valentin Vieriu <valentin.vieriu@gmail.com>
2014-02-16 10:16:04 +0100

    added the cube images and normalised the chart

--- app/scripts/controllers/wait-dashboard.js
+++ app/scripts/controllers/wait-dashboard.js
@@ -13,7 +13,8 @@ angular.module('12oder3App')
 		) {
   			var totalScore = 0;
 			$scope.users = data.fBase.users;
-			$scope.teamScore = {
+			$scope.teamScore = {};
+			$scope.normalizedTeamScore = {
 				'1':0,
 				'2':0,
 				'3':0
@@ -35,15 +36,39 @@ angular.module('12oder3App')
 						
 			},10000);
 
+			$scope.shuffleCube = function(){
+				return _shuffle([1,2,3,4,5]);
+			}
+
 			$scope.users.$on('loaded', function(){
+				var max_score = 250;
+				var divide = 1;
+				var teamScore = {
+					'1':0,
+					'2':0,
+					'3':0
+				};
+				var normalizedTeamScore = {};
 				angular.forEach($scope.users.$getIndex(), function(item){
 					// console.log($scope.users[item].team ,$scope.users[item].score);
+
 					var userScore = $scope.users[item].score || 0;
-					if ($scope.users[item].team == 1) { $scope.teamScore[1] = $scope.teamScore[1] + userScore;};
-					if ($scope.users[item].team == 2) { $scope.teamScore[2] = $scope.teamScore[2] + userScore;};
-					if ($scope.users[item].team == 3) { $scope.teamScore[3] = $scope.teamScore[3] + userScore;};
+					if ($scope.users[item].team == 1) { teamScore[1] = teamScore[1] + userScore;};
+					if ($scope.users[item].team == 2) { teamScore[2] = teamScore[2] + userScore;};
+					if ($scope.users[item].team == 3) { teamScore[3] = teamScore[3] + userScore;};
 					totalScore += item.score;
 				});
-				// console.log($scope.teamScore );
+
+				divide = max_score/Math.max(teamScore[1],teamScore[2],teamScore[3]);
+
+				angular.forEach(teamScore,function(el,index){
+					// console.log(el,divide,index);
+					normalizedTeamScore[index] = Math.floor(el * divide);
+					// console.log(el);
+				});
+				$scope.teamScore = teamScore;
+				$scope.normalizedTeamScore = normalizedTeamScore;
+
+				// console.log($scope.teamScore ,normalizedTeamScore);
 			});
 		});
