f03071270ef80b7203629cbcef3a05337424c266
Valentin Vieriu <vvieriu@sapient.com>
2014-02-17 18:36:31 +0100

    latest pictures

--- app/scripts/controllers/set-user-type.js
+++ app/scripts/controllers/set-user-type.js
@@ -11,6 +11,7 @@ angular.module('12oder3App')
 			var currentUserId;
 			if ($routeParams.userType == 'admin' || $routeParams.userType == 'dashboard') {
 				Localstorage.set('userType',$routeParams.userType);
+				$location.path('/'+$routeParams.userType);
 			}
 			else {
 				$location.path('/');
